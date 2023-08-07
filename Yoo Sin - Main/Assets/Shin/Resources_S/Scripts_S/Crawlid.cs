1eedffd281bc3f18b115d31c7fdeb87e0ceee13d
DESKTOP-I61OQFM\신성창 <ssin011123@gmail.com>
2023-08-08 01:27:33 +0900

    Feat : Add Crawlid Fuction
    - Add Crawlid front Cliff Detected Function
    - Add Crawlid front Crash Detected Function

--- Yoo Sin - Main/Assets/Shin/Resources_S/Scripts_S/Crawlid.cs
+++ Yoo Sin - Main/Assets/Shin/Resources_S/Scripts_S/Crawlid.cs
@@ -9,7 +9,7 @@ public class Crawlid : MonoBehaviour
     public SpriteRenderer crawlidSprite = default;
     public Animator crawlidAnimator = default;
 
-    private float speed = 3.5f;
+    private float speed = 1.5f;
 
     public int vector = default;
 
@@ -38,45 +38,40 @@ public class Crawlid : MonoBehaviour
             crawlidRigid.velocity = crawlidPos;            
         }
 
-        //// 몬스터 앞 찍힐 레이위치 조정하기
+        // 몬스터 앞 찍힐 레이위치 조정하기
         Vector2 front = new Vector2(transform.position.x + 0.5f * vector, transform.position.y);
         Vector2 frontDown = new Vector3(0f, -0.6f);
-        //// 몬스터 앞 찍힐 레이위치 조정하기
+        // 몬스터 앞 찍힐 레이위치 조정하기
 
 
         // 몬스터 앞 레이 찍어보기
         Debug.DrawRay(front, frontDown, Color.yellow);
 
-        //// 플랫폼 위에 있을때만 (공중에서는 레이를 찍지 않는다.) 자신의 전진방향 아래를 레이로 절벽인지 체크한다.    
+        // 플랫폼 위에 있을때만 (공중에서는 레이를 찍지 않는다.) 자신의 전진방향 아래를 레이로 절벽인지 체크한다.    
         if (isGround.Equals(true))
         {
-            RaycastHit2D hit = Physics2D.Raycast(front, frontDown, 1, LayerMask.GetMask("platform"));
+            RaycastHit2D hit = Physics2D.Raycast(front, frontDown, 1, LayerMask.GetMask("Platform"));
 
-            //if(hit.collider == null)
-            //{
-            //    crawlidAnimator.SetTrigger("isTurn");
-            //    Invoke("CrawlidFlip", 0.5f);
-            //    vector *= -1;
-            //}
-
-            //// 콜라이더가 존재하지 않는다면 (절벽이라면)
-            //if (hit.collider == null)
-            //{
-            //    isCliff = true;
-            //}
-            //else if (hit.collider != null)
-            //{
-            //    isCliff = false;
-            //}
+            // 레이가 찍히는 지점에 콜라이더가 없다면 (절벽이라면) 절벽은 참이다.
+            if(hit.collider == null)
+            {
+                isCliff = true;
+                
+            }
+            else if(hit.collider != null)
+            {
+                isCliff = false;
+            }
 
         }
 
-        // 절벽이이거나 충돌이 일어난다면 방향전환을 참으로 바꿔준다.
-        if (isCrash.Equals(true))
+        // 절벽이거나 충돌이 일어난다면 방향전환을 참으로 바꿔준다.
+        if (isCliff.Equals(true) || isCrash.Equals(true))
         {            
+            // 무한 방향전환을 막기위해 충돌의 참 값 초기화, 방향전환동안 crawlid의 이동값을 막기위해 isTurn = true.
             isCrash = false;
-
             isTurn = true;
+
             crawlidAnimator.SetTrigger("isTurn");
 
             // 방향 전환애니메이션 클립이 재생되는 시간만큼 crawlid를 멈춰둔다.
@@ -96,30 +91,35 @@ public class Crawlid : MonoBehaviour
         // crawlid에게 충돌이 발생한 지점의 좌표를 받아내는 contact
         ContactPoint2D contact = collision.contacts[0];
 
-        Debug.LogFormat("충돌체 값 : {0}", collision.contacts[0].point.normalized);
         // crawlid의 자기 포지션 가져오기
         Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
 
         // 충돌체와의 거리 계산
         Vector2 offset = collision.contacts[0].point - myPos;
 
-        Debug.LogFormat("충돌체와의 방향값 : {0}", offset.normalized);
+        // Legacy : 좌표값, 방향값 확인용 Log
+        // Debug.LogFormat("충돌체 값 : {0}", collision.contacts[0].point.normalized);
+        // Debug.LogFormat("충돌체와의 방향값 : {0}", offset.normalized);
 
 
+        // 충돌 지점이 일정높이 이상이라면 ( 전방에서 충돌이 일어났다면 ) 충돌값을 참으로 바꿔준다.
         if(offset.normalized.y >= -0.2f)
         {
             isCrash = true;
         }
 
+        // 공중에서 무한절벽 탐지를 막기위한 isGround
         if (collision.collider.tag == "Platform")
         {
             isGround = true;
         }
+
     }
 
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        // 공중에서 무한절벽 탐지를 막기위한 isGround
         if (collision.collider.tag == "Platform")
         {
             isGround = false;
@@ -129,7 +129,7 @@ public class Crawlid : MonoBehaviour
 
     public void CrawlidFlip()
     {
-
+        // 기본 스프라이트가 왼쪽방향 (-1 방향) 을 바라보기에 vector 값이 음수면 플립 false, 양수면 플립 true.
         if (vector <= 0)
         {
             crawlidSprite.flipX = false;
