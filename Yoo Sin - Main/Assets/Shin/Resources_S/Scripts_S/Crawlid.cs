857f5c05a74dd75f786725e1e77836de206968e5
DESKTOP-51TGSF5\KGA <dlwnsdud0612@gmail.com>
2023-08-14 20:23:56 +0900

    Refactor : Script Crawlid
    - Refactored Script Crawlid
    - Add opposite AddForce Function
    - Comment Script Crawlid

--- Yoo Sin - Main/Assets/Shin/Resources_S/Scripts_S/Crawlid.cs
+++ Yoo Sin - Main/Assets/Shin/Resources_S/Scripts_S/Crawlid.cs
@@ -1,239 +1,214 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Crawlid : MonoBehaviour
 {
-    public Transform playerTransform = default;
+    private Transform player = default;
     private Rigidbody2D crawlidRigid = default;
-    private SpriteRenderer crawlidSprite = default;
-    private Animator crawlidAnimator = default;
     private CapsuleCollider2D crawlidCollider = default;
+    private Animator crawlidAnimator = default;
+    private SpriteRenderer crawlidSprite = default;
 
-    private float speed = 3.0f;
+    private Vector2 myPos = default;    
+    private float vector = -1;
+    private float speed = 2.0f;
     private int lifeCount = 3;
 
-    public int vector = default;
-
+    public bool isDie = false;
     public bool isTurn = false;
     public bool isGround = false;
-    public bool isCliff = false;
-    public bool isCrash = false;
-    public bool isDead = false;
     public bool isHit = false;
 
-    // Start is called before the first frame update
     void Awake()
-    {        
+    {
+        player =GameObject.FindWithTag("Player").GetComponent<Transform>();
         crawlidRigid = GetComponent<Rigidbody2D>();
-        crawlidSprite = GetComponent<SpriteRenderer>();
-        crawlidAnimator = GetComponent<Animator>();
         crawlidCollider = GetComponent<CapsuleCollider2D>();
+        crawlidAnimator  = GetComponent<Animator>();
+        crawlidSprite = GetComponent<SpriteRenderer>();
 
-        // Crawlid 의 전진방향, 기본 스프라이트가 왼쪽을 바라보는 방향이기에 -1 값을 기본으로 주었음.
-        vector = -1;
+        isGround = true;        
     }
 
     private void FixedUpdate()
     {
+        myPos = transform.position;
 
-        // crawlid 의 행동은 살아있을 때만 동작한다.
-        if(isDead.Equals(false))
+        // Crawlid의 이동 로직 == 죽지않았을 때, 돌고있는 상태가 아닐 때, 맞고있을때가 아닐 때 한 방향으로 움직인다.
+        if (isDie == false &&isTurn == false && isHit == false)
         {
-            // 플랫폼 위에 있으면서 방향전환을 하고 있는것이 아니라면 해당 방향으로 전진한다.
-            if (isGround.Equals(true) && isTurn.Equals(false))
-            {
-                Vector2 crawlidPos = new Vector2(speed * vector, crawlidRigid.velocity.y);
-                crawlidRigid.velocity = crawlidPos;            
-            }
-
-
-            // { 몬스터 앞 찍힐 레이위치 조정하기
-            Vector2 front = new Vector2(transform.position.x + 0.6f * vector, transform.position.y);
-            Vector2 frontDown = new Vector3(0f, -0.6f);
-
-            // 몬스터 앞 레이 찍어보기
-            Debug.DrawRay(front, frontDown, Color.yellow);
-
-            // } 몬스터 앞 찍힐 레이위치 조정하기
-
-            
-            // 플랫폼 위에 있을때만 (공중에서는 레이를 찍지 않는다.) 자신의 전진방향 아래를 레이로 절벽인지 체크한다.    
-            if (isGround.Equals(true))
-            {
-                RaycastHit2D hit = Physics2D.Raycast(front, frontDown, 1, LayerMask.GetMask("Platform"));
-
-                // 레이가 찍히는 지점에 콜라이더가 없다면 (절벽이라면) 절벽은 참이다.
-                if(hit.collider == null)
-                {
-                    isCliff = true;
-                
-                }
-                else if(hit.collider != null)
-                {
-                    isCliff = false;
-                }
-
-            }
-
-
-            // 절벽이거나 충돌이 일어난다면 방향전환을 참으로 바꿔준다.
-            if (isCliff.Equals(true) || isCrash.Equals(true))
-            {            
-                // 무한 방향전환을 막기위해 충돌의 참 값 초기화, 방향전환동안 crawlid의 이동값을 막기위해 isTurn = true.
-                isCrash = false;
-                isTurn = true;
-
-                crawlidAnimator.SetTrigger("isTurn");
-
-                // 방향 전환애니메이션 클립이 재생되는 시간만큼 crawlid를 멈춰둔다.
-                crawlidRigid.velocity = Vector2.zero;
-
-                // crawlid를 멈춰둘 시간, 스프라이트 뒤집을 함수 CrawlidFlip 호출
-                Invoke("CrawlidFlip", 0.5f);
-
-                // 전진방향 전환을 위해 -1을 곱하여 방향을 다시 잡아준다.
-                vector *= -1;
-            }
-
+            crawlidRigid.velocity = new Vector2(speed * vector, crawlidRigid.velocity.y);        
         }
 
+
+        // *{ 레이캐스트로 절벽 탐지하는 로직
+
+        // 레이캐스트의 시작점 == crawlid보다 x값이 0.6앞선 위치 * vector로 왼쪽인지 오른쪽인지 판별한다.
+        Vector2 crawlidFront = new Vector2(transform.position.x + 0.6f * vector, transform.position.y);
+        // 레이캐스트의 끝점 == 시작점에의 y값의 -1f한 지점까지 찍을예정
+        Vector2 crawlidFrontdown = new Vector2(0, -1f);
+
+        // 레이캐스트가 찍히는곳을 직접 봐보기
+        // Debug.DrawRay(crawlidFront, crawlidFrontdown, Color.green);
+ 
+        RaycastHit2D hit = Physics2D.Raycast(crawlidFront, crawlidFrontdown, 1, LayerMask.GetMask("Platform"));
+
+        // 레이캐스트가 찍은곳에 플랫폼이 없으며 현재 플랫폼 위에 있는 상태라면
+        if (isGround == true && hit.collider == null) 
+        {
+            StartCoroutine(Turning());
+        }
+
+        // *} 레이캐스트로 절벽 탐지하는 로직
+
     }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(isDead.Equals(true))
-        {            
-            if(collision.collider.tag.Equals("Platform"))
-            {
-                crawlidRigid.simulated = false;
-                crawlidCollider.enabled = false;
+        // 충돌 지점의 좌표를 받아내는 crashedPoint
+        Vector2 crashedPoint = collision.contacts[0].point;
+        Vector2 crashedNormalize = crashedPoint - myPos;
 
-                crawlidAnimator.SetTrigger("Die_Land");
-            }
-
-        }
-        else
+        // 죽은상태에서는 포물선으로 날아가게 설정, 날아가는 도중 플랫폼에 닿으면
+        if(isDie == true && collision.collider.tag.Equals("Platform") && crashedNormalize.y < -0.2f)
         {
-            // crawlid에게 충돌이 발생한 지점의 좌표를 받아내는 contact
-            ContactPoint2D contact = collision.contacts[0];
+            crawlidAnimator.SetTrigger("Die_Land");
 
-            // crawlid의 자기 포지션 가져오기
-            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+            Debug.Log(crashedNormalize);
+            // Rigidbody 비활성화
+            crawlidRigid.simulated = false;
+            // 충돌을 제외하기 위한 Collider 비활성화
+            crawlidCollider.enabled = false;
+        }
 
-            // 충돌체와의 거리 계산
-            Vector2 offset = collision.contacts[0].point - myPos;
-
-            // Legacy : 좌표값, 방향값 확인용 Log
-            // Debug.LogFormat("충돌체 값 : {0}", collision.contacts[0].point.normalized);
-            // Debug.LogFormat("충돌체와의 방향값 : {0}", offset.normalized);
-
-            // 공중에서 무한절벽 탐지를 막기위한 isGround
-            if (collision.collider.tag == "Platform")
+        // 충돌한것이 플레이어가 아니며
+        if(collision.collider.tag != "Player")
+        {
+            // crawlid의 아랫방향에 있는것이 아니라면 (땅바닥을 제외하려고) 방향전환을 한다.
+            if (crashedNormalize.y > -0.2f && isHit == false)
             {
-                isGround = true;
+                StartCoroutine(Turning());
             }
+        }
 
-            // 충돌 지점이 일정높이 이상이라면 ( 전방에서 충돌이 일어났다면 ) 충돌값을 참으로 바꿔준다.
-            if(collision.collider.tag.Equals("Player"))
-            {
-                // 충돌한것이 플레이어라면
-                /* Pass */
-            }
-            else if(offset.normalized.y >= -0.2f)
-            {
-                isCrash = true;
-            }
-
+        // 충돌한것이 플랫폼이라면 isGround = true
+        if(collision.collider.tag == "Platform")
+        {
+            isGround = true;
         }
 
     }
 
-
+    // 플랫폼에서 벗어나는 순간 isGround = false
     private void OnCollisionExit2D(Collision2D collision)
     {
-        // 공중에서 무한절벽 탐지를 막기위한 isGround
-        if (collision.collider.tag == "Platform")
+        if(collision.collider.tag == "Platform")
         {
             isGround = false;
-        }
-
+        }        
     }
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
-    {    
-        
-        // crawlid 가 살아있는 동안 공격을 받는다면
-        if (isDead.Equals(false) && collision.tag.Equals("PlayerAttack"))
+    {
+        // 플레이어의 공격에 닿게 되면
+        if(collision.tag.Equals("PlayerAttack"))
         {
-            Debug.Log("Crawlid 타격");
-            lifeCount -= 1;            
+            // 라이프 카운트가 1씩 깎이며
+            lifeCount -= 1;
 
-            Vector3 myPos = new Vector2(transform.position.x, transform.position.y);
-            Vector2 offset = playerTransform.position - myPos;
-
-            //Debug.LogFormat("몬스터로부터 플레이어 방향 : {0}", offset.normalized);
+            // 현재 플레이어와의 거리를 계산하고
+            Vector2 offset = new Vector2(player.position.x - myPos.x, player.position.y - myPos.y);
 
 
-            /*
-                ========= Legacy : Crawlid 타격시 플레이어 반대방향으로 밀려나는 기능 추가하기 ========
-             
-            if (offset.normalized.x < 0)
-            {
-                crawlidRigid.AddForce(transform.right * 5f, ForceMode2D.Impulse);
-            }
-            else if (offset.normalized.x > 0)
-            {
-                
-            }
-                ========= Legacy : Crawlid 타격시 플레이어 반대방향으로 밀려나는 기능 추가하기 ========
-            */
-
-            if (lifeCount <= 0)
-            {
+            // 라이프 카운트가 0 이하가 되면
+            if(lifeCount <= 0)
+            {            
+                // isDie = true , 속력을 잃고, 죽는 애니메이션 재생
+                isDie = true;
                 crawlidRigid.velocity = Vector2.zero;
-                isDead = true;
+                crawlidAnimator.SetTrigger("Die");
 
-                crawlidAnimator.SetTrigger("Die");                          
+                // 만약 플레이어가 왼쪽방향에서 가격하여 사망하게 된경우
+                if (offset.normalized.x < 0f && (offset.normalized.y > -0.3f && offset.normalized.y < 0.3f))
+                {
+                    // 오른쪽 윗 대각선으로 날아간다.
+                    Vector2 hitForce = new Vector2(10f, 2.5f);
+                    crawlidRigid.AddForce(hitForce, ForceMode2D.Impulse);
+                }
+                else if (offset.normalized.x > 0f && (offset.normalized.y > -0.3f && offset.normalized.y < 0.3f))
+                {
+                    // 플레이어가 오른쪽 방향에서 가격하여 사망하게 된 경우 왼쪽 윗 대각선으로 날아간다.
+                    Vector2 hitForce = new Vector2(-10f, 2.5f);
+                    crawlidRigid.AddForce(hitForce, ForceMode2D.Impulse);
+                }
 
-                if(offset.normalized.x < 0 && (offset.normalized.y > -0.3f && offset.normalized.y < 0.3f))
-                {                    
-                    Vector2 attackForce = new Vector2(5f, 5f);
-                    crawlidRigid.AddForce(attackForce, ForceMode2D.Impulse);
-                }
-                else if(offset.normalized.x > 0 && (offset.normalized.y > -0.3f && offset.normalized.y < 0.3f))
-                {
-                    Vector2 attackForce = new Vector2(5f *-1, 5f);
-                    crawlidRigid.AddForce(attackForce, ForceMode2D.Impulse);
-                }
-                else
-                {
-                    crawlidRigid.simulated = false;
-                    crawlidCollider.enabled = false;                        
-                }
-                
+
 
             }
-
-            isHit = false;
+            else if (offset.normalized.x < 0f)
+            {
+                // 살아있는 상태, 플레이어가 왼쪽에서 가격한 경우 오른쪽으로 타격하는 힘을 줌
+                crawlidRigid.velocity = Vector2.zero;
+                crawlidRigid.AddForce(transform.right * 5f, ForceMode2D.Impulse);
+                StartCoroutine(Hit());
+            }
+            else if(offset.normalized.x > 0f)
+            {
+                // 살아있는 상태, 플레이어가 오른쪽에서 가격한 경우 오른쪽으로 타격하는 힘을 줌
+                crawlidRigid.velocity = Vector2.zero;
+                crawlidRigid.AddForce(transform.right * -5f, ForceMode2D.Impulse);
+                StartCoroutine(Hit());
+            }
         }
     }
 
-    public void CrawlidFlip()
+
+    // crawlid의 방향 전환을 하는 상태
+    // FixedUpdate와 OnCollsionEnter2D 함수 내부 존재
+    IEnumerator Turning()
+    {        
+        crawlidRigid.velocity = Vector2.zero;
+        isTurn = true;
+        vector *= -1;
+
+        crawlidAnimator.SetTrigger("isTurn");
+        yield return new WaitForSeconds(0.5f);
+
+        HorizontalFlip();
+        isTurn = false;
+        
+
+    }
+
+    // crawlid가 공격받는 상태
+    // OnTriggerEnter2D 함수 내부 존재
+    IEnumerator Hit()
     {
-        // 기본 스프라이트가 왼쪽방향 (-1 방향) 을 바라보기에 vector 값이 음수면 플립 false, 양수면 플립 true.
-        if (vector <= 0)
-        {
-            crawlidSprite.flipX = false;
-        }
-        else
+        isHit = true;
+
+        yield return new WaitForSeconds(0.25f);
+
+        isHit = false;
+    }
+
+    // crawlid의 방향 방향 전환
+    // IEnumerator Turning 함수 내부존재
+    void HorizontalFlip()
+    {
+        if(vector > 0)
         {
             crawlidSprite.flipX = true;
         }
-
-        isTurn = false;
-
+        else if(vector < 0)
+        {
+            crawlidSprite.flipX = false;
+        }
     }
-
 }
